d25698828aaf1a0cb63d340be84dc297e5a81a44
pareronia <49491686+pareronia@users.noreply.github.com>
2022-02-21 01:41:00 +0100

    nl.infi.aoc 2021 - fix formatting

--- src/main/java/nl/infi/aoc/AoC2021.java
+++ src/main/java/nl/infi/aoc/AoC2021.java
@@ -75,12 +75,12 @@ zetten. In het bovenstaande voorbeeld zou dat dus <code>FZZ</code> worden.
 Er zijn al 20 cadeaus ingepakt. Gegeven het aantal missende <br/>
 onderdelen in de speelgoedlijst, wat zijn dan de beginletters (op <br/>
 alfabetische volgorde)?
- */
+*/
 public class AoC2021 extends AocBase {
 
     private final int missen;
-    private final Map<String, List<Pair<Integer, String>>> speelgoeden;
-    
+private final Map<String, List<Pair<Integer, String>>> speelgoeden;
+
     private AoC2021(final boolean debug, final String input) {
         super(debug);
         final String[] lines = input.split("\\r?\\n");
@@ -98,7 +98,7 @@ public class AoC2021 extends AocBase {
             })
             .collect(groupingBy(Pair::getLeft, mapping(Pair::getRight, toList())));
     }
-    
+
     private int aantalOnderdelen(final String speelgoed) {
         if (!this.speelgoeden.keySet().contains(speelgoed)) {
             return 1;
@@ -108,76 +108,76 @@ public class AoC2021 extends AocBase {
                 .sum();
     }
 
-	@Override
+    @Override
     public Integer solvePart1() {
-	    return this.speelgoeden.keySet().stream()
-	        .mapToInt(this::aantalOnderdelen)
-	        .max().orElseThrow();
+        return this.speelgoeden.keySet().stream()
+            .mapToInt(this::aantalOnderdelen)
+            .max().orElseThrow();
     }
-	
-	private Stream<String> rest(final Map<Integer, String> itos, final int max) {
-	    return itos.entrySet().stream()
-	        .filter(e -> e.getKey() <= max)
-	        .sorted((e1, e2) -> Integer.compare(e2.getKey(), e1.getKey()))
-	        .map(Entry::getValue);
-	}
-	
-	private String encodeer(final String[] a) {
-	    return Arrays.stream(a)
-	            .filter(Objects::nonNull)
-	            .map(s -> s.substring(0, 1))
-	            .sorted()
-	            .collect(joining());
-	}
-	
-	private boolean dfs(
-	        final Set<String> klaar,
-	        final String[] a,
-	        final int i,
-	        final Map<Integer, String> itos,
-	        final Map<String, Integer> stoi
-	    ) {
-	    final int totaal = Arrays.stream(a)
-	            .filter(Objects::nonNull)
-	            .mapToInt(s -> stoi.get(s).intValue())
-	            .sum();
-	    if (i == a.length) {
-	        return totaal == this.missen;
-	    }
+
+    private Stream<String> rest(final Map<Integer, String> itos, final int max) {
+        return itos.entrySet().stream()
+                .filter(e -> e.getKey() <= max)
+                .sorted((e1, e2) -> Integer.compare(e2.getKey(), e1.getKey()))
+                .map(Entry::getValue);
+    }
+
+    private String encodeer(final String[] a) {
+        return Arrays.stream(a)
+                .filter(Objects::nonNull)
+                .map(s -> s.substring(0, 1))
+                .sorted()
+                .collect(joining());
+    }
+
+    private boolean dfs(
+            final Set<String> klaar,
+            final String[] a,
+            final int i,
+            final Map<Integer, String> itos,
+            final Map<String, Integer> stoi
+        ) {
+        final int totaal = Arrays.stream(a)
+                .filter(Objects::nonNull)
+                .mapToInt(s -> stoi.get(s).intValue())
+                .sum();
+        if (i == a.length) {
+            return totaal == this.missen;
+        }
         for (final String s : rest(itos, this.missen - totaal).collect(toSet())) {
-	        a[i] = s;
-	        final String e = encodeer(a);
+            a[i] = s;
+            final String e = encodeer(a);
             if (!klaar.contains(e)) {
-	            klaar.add(e);
+                klaar.add(e);
                 if (dfs(klaar, a, i + 1, itos, stoi)) {
                     return true;
                 }
             }
-	        a[i] = null;
-	    }
-	    return false;
-	}
-	
-	private String solve2(final int ingepakt) {
-	    final Set<String> onderdelen = this.speelgoeden.values().stream()
-	            .flatMap(v -> v.stream())
-	            .map(Pair::getRight)
-	            .collect(toSet());
-	    final Map<Integer, String> itos = this.speelgoeden.keySet().stream()
-	            .filter(s -> !onderdelen.contains(s))
-	            .collect(toMap(this::aantalOnderdelen, d -> d));
-	    final Map<String, Integer> stoi = itos.entrySet().stream()
-	        .collect(toMap(Entry::getValue, Entry::getKey));
-	    log(stoi);
-	    final HashSet<String> klaar = new HashSet<>();
-	    final String[] a = new String[ingepakt];
+            a[i] = null;
+        }
+        return false;
+    }
+
+    private String solve2(final int ingepakt) {
+        final Set<String> onderdelen = this.speelgoeden.values().stream()
+                .flatMap(v -> v.stream())
+                .map(Pair::getRight)
+                .collect(toSet());
+        final Map<Integer, String> itos = this.speelgoeden.keySet().stream()
+                .filter(s -> !onderdelen.contains(s))
+                .collect(toMap(this::aantalOnderdelen, d -> d));
+        final Map<String, Integer> stoi = itos.entrySet().stream()
+            .collect(toMap(Entry::getValue, Entry::getKey));
+        log(stoi);
+        final HashSet<String> klaar = new HashSet<>();
+        final String[] a = new String[ingepakt];
         if (dfs(klaar, a, 0, itos, stoi)) {
-	        log(klaar.size());
-	        log(Arrays.stream(a).collect(joining(", ")));
-	        return encodeer(a);
-	    }
-	    throw new IllegalStateException("Unsolvable");
-	}
+            log(klaar.size());
+            log(Arrays.stream(a).collect(joining(", ")));
+            return encodeer(a);
+        }
+        throw new IllegalStateException("Unsolvable");
+    }
 
     @Override
     public String solvePart2() {
@@ -185,41 +185,41 @@ public class AoC2021 extends AocBase {
     }
 
     public static final AoC2021 create(final String input) {
-		return new AoC2021(false, input);
-	}
+        return new AoC2021(false, input);
+    }
 
-	public static final AoC2021 createDebug(final String input) {
-		return new AoC2021(true, input);
-	}
-	
-	public static void main(final String[] args) throws Exception {
-	    assert AoC2021.createDebug(TEST).solvePart1() == 18;
-	    assert AoC2021.createDebug(TEST).solve2(3).equals("FZZ");
-		lap("Part 1", () -> AoC2021.create(INPUT).solvePart1());
-		lap("Part 2", () -> AoC2021.create(INPUT).solvePart2());
-	}
-	
-	private static final String TEST =
-	        "46 onderdelen missen\r\n"
-	        + "Zoink: 9 Oink, 5 Dink\r\n"
-	        + "Floep: 2 Flap, 4 Dink\r\n"
-	        + "Flap: 4 Oink, 3 Dink";
-	private static final String INPUT =
-	        "1966115 onderdelen missen\r\n"
-	        + "Lightsaber: 59 Batterij, 19 Unobtanium\r\n"
-	        + "HandheldComputer: 29 Batterij, 5 Printplaat, 5 Plastic\r\n"
-	        + "ElectrischeRacebaan: 59 AutoChassis, 43 Printplaat, 47 Plastic, 89 Batterij, 67 Wiel\r\n"
-	        + "QuadDrone: 41 Accu, 5 Plastic, 79 Printplaat\r\n"
-	        + "PikachuPlushy: 5 Batterij\r\n"
-	        + "Trampoline: 17 Schokdemper, 31 IJzer\r\n"
-	        + "BatmobileReplica: 97 BatmobileChassis, 29 Schokdemper, 89 Unobtanium, 61 Wiel\r\n"
-	        + "DanceDanceRevolutionMat: 31 Schokdemper, 37 Batterij\r\n"
-	        + "Printplaat: 41 Hars, 73 Koper, 83 Chip, 41 Led\r\n"
-	        + "Accu: 71 Batterij\r\n"
-	        + "Schokdemper: 61 IJzer, 59 Staal\r\n"
-	        + "Batterij: 43 Staal\r\n"
-	        + "BatmobileChassis: 71 AutoChassis, 47 Staal\r\n"
-	        + "AutoChassis: 73 IJzer\r\n"
-	        + "Wiel: 11 Rubber, 37 IJzer\r\n"
-	        + "Unobtanium: 43 IJzer, 97 Kryptonite";
+    public static final AoC2021 createDebug(final String input) {
+        return new AoC2021(true, input);
+    }
+
+    public static void main(final String[] args) throws Exception {
+        assert AoC2021.createDebug(TEST).solvePart1() == 18;
+        assert AoC2021.createDebug(TEST).solve2(3).equals("FZZ");
+        lap("Part 1", () -> AoC2021.create(INPUT).solvePart1());
+        lap("Part 2", () -> AoC2021.create(INPUT).solvePart2());
+    }
+
+    private static final String TEST =
+            "46 onderdelen missen\r\n"
+          + "Zoink: 9 Oink, 5 Dink\r\n"
+          + "Floep: 2 Flap, 4 Dink\r\n"
+          + "Flap: 4 Oink, 3 Dink";
+    private static final String INPUT =
+            "1966115 onderdelen missen\r\n"
+          + "Lightsaber: 59 Batterij, 19 Unobtanium\r\n"
+          + "HandheldComputer: 29 Batterij, 5 Printplaat, 5 Plastic\r\n"
+          + "ElectrischeRacebaan: 59 AutoChassis, 43 Printplaat, 47 Plastic, 89 Batterij, 67 Wiel\r\n"
+          + "QuadDrone: 41 Accu, 5 Plastic, 79 Printplaat\r\n"
+          + "PikachuPlushy: 5 Batterij\r\n"
+          + "Trampoline: 17 Schokdemper, 31 IJzer\r\n"
+          + "BatmobileReplica: 97 BatmobileChassis, 29 Schokdemper, 89 Unobtanium, 61 Wiel\r\n"
+          + "DanceDanceRevolutionMat: 31 Schokdemper, 37 Batterij\r\n"
+          + "Printplaat: 41 Hars, 73 Koper, 83 Chip, 41 Led\r\n"
+          + "Accu: 71 Batterij\r\n"
+          + "Schokdemper: 61 IJzer, 59 Staal\r\n"
+          + "Batterij: 43 Staal\r\n"
+          + "BatmobileChassis: 71 AutoChassis, 47 Staal\r\n"
+          + "AutoChassis: 73 IJzer\r\n"
+          + "Wiel: 11 Rubber, 37 IJzer\r\n"
+          + "Unobtanium: 43 IJzer, 97 Kryptonite";
 }
